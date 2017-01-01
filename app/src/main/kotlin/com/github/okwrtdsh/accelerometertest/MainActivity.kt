856439d3f4bcf097115e5207ad8e52ad9518e2f5
okwrtdsh <okwrtdsh@gmail.com>
2017-01-01 23:53:02 +0900

    kotlin android Accelerometer Sensor example

---
+++ app/src/main/kotlin/com/github/okwrtdsh/accelerometertest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.github.okwrtdsh.accelerometertest
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import butterknife.bindView
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    val textView: TextView by bindView(R.id.text_view)
+    val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        textView.text = event!!.values.zip("XYZ".toList()).fold("") { acc, pair ->
+            "$acc${pair.second}: ${pair.first}\n"
+        }
+    }
+}
